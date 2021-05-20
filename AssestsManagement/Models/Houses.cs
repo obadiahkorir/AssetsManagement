b1be2ca27bc9dd50ec95a94f69f16177ba125a4b
Obadiah Korir <okorir@developmentpathways.co.uk>
2021-05-20 12:17:04 +0300

    This is the first commit

---
+++ AssestsManagement/Models/Houses.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AssestsManagement.Models
+{
+    public class Houses
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string County  { get; set; }
+        public int NoofRooms { get; set; }
+    }
+
+}
